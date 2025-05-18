7955a3e310c7f0de3b63e8310e8e1f4139e0f3c2
Vinicios <vinicios.ldcampos@gmail.com>
2025-05-17 21:50:49 -0300

    Criando componentes para o campo de repositories - RepositoryItem e a função repositoryItem

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as S from "./styled"
+import RepositoryItem from '../repository-item';
 
 const Repositories = () => {
   return (
@@ -11,8 +12,20 @@ const Repositories = () => {
             <S.WrapperTab>Repositories</S.WrapperTab>
             <S.WrapperTab>Starred</S.WrapperTab>
         </S.WrapperTabList>
-        <S.WrapperTabPanel> Panel Repositories</S.WrapperTabPanel>
-        <S.WrapperTabPanel> Panel Starred</S.WrapperTabPanel>
+        <S.WrapperTabPanel>
+            <RepositoryItem 
+                name ="bootcampImpulso_TypeScript" 
+                linkToRepo="https://github.com/viniciosldcampos"
+                fullName="viniciosldcampos/bootcampImpulso_TypeScript"
+            />
+        </S.WrapperTabPanel>
+        <S.WrapperTabPanel>
+            <RepositoryItem
+                name ="Contador_Bootcamp_Impulso_DIO" 
+                linkToRepo="https://github.com/viniciosldcampos"
+                fullName="viniciosldcampos/Contador_Bootcamp_Impulso_DIO"
+            />
+        </S.WrapperTabPanel>
     </S.WrapperTabs>
   );
 };
